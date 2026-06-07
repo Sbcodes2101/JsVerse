29e730ba282b4cb4cfee3dffb09f54516870497a
Sbcodes2101 <sarthakbahuguna7269@gmail.com>
2026-06-07 11:33:56 +0530

    to temp

--- src/Components/Ending.jsx
+++ src/Components/Ending.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import LiquidText from "./LiquidText";
+import LiquidText from "./Temp";
 
 gsap.registerPlugin(ScrollTrigger);
 
